2a9e529642f207ee8450d09da8de7903289e1d5f
Va11ery <109084396+Va11ery@users.noreply.github.com>
2022-08-31 21:41:30 +0000

    Deploying to gh-pages from @ Va11ery/site_visit_magura@58237dd6cfd1f53aca3891cc7a74add3abb0f2da 🚀

---
+++ assets/AboutPage.b344435b.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.8dbd43fc.js";import{_ as a,F as t,G as s,H as o,I as r,O as n}from"./index.54e90cd4.js";import"./render.3dcdf4e4.js";const c=t({name:"AboutPage"}),d=n("div",null,"dasdaasdaas",-1);function _(f,p,i,l,m,u){return s(),o(e,{class:"flex flex-center"},{default:r(()=>[d]),_:1})}var h=a(c,[["render",_]]);export{h as default};
